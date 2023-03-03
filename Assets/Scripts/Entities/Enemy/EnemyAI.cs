4b61ee4aa24d1934a386052f798a9b5f3624c066
Thomas Presicci <thomaspresicci@gmail.com>
2023-03-03 17:25:27 -0600

    Moved hit handling to EntityDamageable

--- Assets/Scripts/Entities/Enemy/EnemyAI.cs
+++ Assets/Scripts/Entities/Enemy/EnemyAI.cs
@@ -7,31 +7,5 @@ using UnityEngine;
 /// <remarks>Thomas Presicci - https://github.com/Presicci</remarks>
 public class EnemyAI : MonoBehaviour
 {
-    public float forceMultiplier;
-    private Rigidbody2D _rigidbody2D;
-    private bool _invulnerability;
     
-    private void Awake()
-    {
-        _rigidbody2D = GetComponent<Rigidbody2D>();
-    }
-
-    private void OnTriggerEnter2D(Collider2D col)
-    {
-        if (_invulnerability) return;
-        Debug.Log("HIT!");
-        // TODO damage handling
-        // TODO knock away from player instead of collider
-        _rigidbody2D.AddForce(forceMultiplier * (transform.position - col.transform.position).normalized, ForceMode2D.Impulse);
-        if (col.CompareTag("PlayerProjectile"))
-            Destroy(col.gameObject);
-        StartCoroutine(InvulnerabilityFrames());
-    }
-    
-    private IEnumerator InvulnerabilityFrames()
-    {
-        _invulnerability = true;
-        yield return new WaitForSeconds(0.1f);
-        _invulnerability = false;
-    }
 }
---
+++ Assets/Scripts/Entities/EntityDamageable.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using UnityEngine;
+
+public class EntityDamageable : MonoBehaviour
+{
+    [SerializeField] public int maxHealth;
+    
+    public float forceMultiplier;
+
+    public int CurrentHealth { private set; get; }
+    private bool _invulnerability;
+    private Rigidbody2D _rigidbody2D;
+
+    private void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        CurrentHealth = maxHealth;
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (_invulnerability) return;
+        Debug.Log("HIT!");
+        Damage(20);
+        _rigidbody2D.AddForce(forceMultiplier * (transform.position - col.transform.position).normalized, ForceMode2D.Impulse);
+        if (col.CompareTag("PlayerProjectile"))
+            Destroy(col.gameObject);
+    }
+    
+    private void Damage(int damage)
+    {
+        if ((CurrentHealth -= damage) <= 0)
+        {
+            Die();
+        }
+        StartCoroutine(InvulnerabilityFrames());
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+    
+    private IEnumerator InvulnerabilityFrames()
+    {
+        _invulnerability = true;
+        yield return new WaitForSeconds(0.1f);
+        _invulnerability = false;
+    }
+}
