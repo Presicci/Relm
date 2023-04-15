96da41808994c66e1f8ab21284ceadbc35318542
Thomas Presicci <thomaspresicci@gmail.com>
2023-04-15 16:07:56 -0500

    Added PlayerAttributes component for managing the player's attribute multipliers

---
+++ Assets/Scripts/Entities/Player/PlayerAttributes.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttributes : MonoBehaviour
+{
+        private Dictionary<AttributeType, float> _attributes;
+        
+        private void Awake()
+        {
+                _attributes = new Dictionary<AttributeType, float>();
+                foreach (AttributeType attribute in Enum.GetValues(typeof(AttributeType)))
+                {
+                        _attributes.Add(attribute, 1f);
+                }
+        }
+
+        public float GetAttributeValue(AttributeType type)
+        {
+                return _attributes[type];
+        }
+
+        public void IncrementAttribute(AttributeType type, float value)
+        {
+                _attributes[type] += value;
+                Debug.Log(type + " upgraded by " + value + " to " + _attributes[type]);
+        }
+}
+
