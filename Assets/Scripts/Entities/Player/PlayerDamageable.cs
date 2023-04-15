06a3d2d3ff68ea50db29d6eec67ed1e01f200e29
Thomas Presicci <thomaspresicci@gmail.com>
2023-04-15 16:38:30 -0500

    Doubled player iFrames and added a little damage taken/iFrame animation

--- Assets/Scripts/Entities/Player/PlayerDamageable.cs
+++ Assets/Scripts/Entities/Player/PlayerDamageable.cs
@@ -3,7 +3,14 @@ using UnityEngine;
 
 public class PlayerDamageable : Damageable
 {
+    [SerializeField] private GameObject gameOverScreen;
+    private SpriteRenderer _spriteRenderer;
     private bool _invulnerability;
+
+    private void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     private void OnCollisionEnter2D(Collision2D col)
     {
@@ -14,7 +21,8 @@ public class PlayerDamageable : Damageable
 
     protected override void Die()
     {
-        throw new System.NotImplementedException();
+        Time.timeScale = 0f;
+        gameOverScreen.SetActive(true);
     }
 
     protected override void OnDamageTaken()
@@ -25,7 +33,17 @@ public class PlayerDamageable : Damageable
     private IEnumerator InvulnerabilityFrames()
     {
         _invulnerability = true;
-        yield return new WaitForSeconds(0.5f);
+        float time = 0f;
+        while (time < 1f)
+        {
+            Color color = time > 0.5f 
+                ? Color.Lerp(Color.red, Color.white, Mathf.PingPong((time - 0.5f) * 2, 1)) 
+                : Color.Lerp(Color.white, Color.red, Mathf.PingPong(time * 2, 1));
+            _spriteRenderer.color = color;
+            time += 0.1f;
+            yield return new WaitForSeconds(0.1f);
+        }
+        _spriteRenderer.color = Color.white;
         _invulnerability = false;
     }
 }
