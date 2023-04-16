1a0892175beee03bf7055b56e217ca801872c5fe
Thomas Presicci <thomaspresicci@gmail.com>
2023-04-16 13:16:18 -0500

    Added support for multiple enemies and added rat and scorpion

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Tilemaps;
@@ -7,15 +8,18 @@ using Random = UnityEngine.Random;
 
 public class EnemyController : MonoBehaviour
 {
-    [SerializeField] private EnemyDumbAI enemyPrefab;
+    [SerializeField] private EnemyDumbAI tier1Enemy;
+    [SerializeField] private EnemyDumbAI tier2Enemy;
+    [SerializeField] private EnemyDumbAI tier3Enemy;
     [SerializeField] private TilemapRenderer tilemapRenderer;
     [SerializeField] private Player player;
-
+    
+    private GameTimer _timer;
     private readonly List<EnemyDumbAI> _activeEnemies = new List<EnemyDumbAI>();
-    private int _spawnAmount = 3;
 
     void Start()
     {
+        _timer = GetComponent<GameTimer>();
         StartCoroutine(SpawnEnemies());
     }
 
@@ -35,17 +39,26 @@ public class EnemyController : MonoBehaviour
 
     private IEnumerator SpawnEnemies()
     {
+        List<EnemyDumbAI> possibleEnemies = new List<EnemyDumbAI> { tier1Enemy };
         while (true)
         {
             yield return new WaitForSeconds(0.25f);
-            for (int index = 0; index < _spawnAmount; index++)
+            if (possibleEnemies.Count == 1 && _timer.GetTime() >= 180f)
+            {
+                possibleEnemies.Add(tier2Enemy);
+            } else if (possibleEnemies.Count == 2 && _timer.GetTime() >= 300f)
+            {
+                possibleEnemies.Add(tier3Enemy);
+            }
+            int spawnAmount = (int) (_timer.GetTime() / 300) + 1;
+            for (int index = 0; index < spawnAmount; index++)
             {
                 Vector3 offset = Random.insideUnitCircle.normalized * 9f;
                 while (!tilemapRenderer.bounds.Contains(player.transform.position + offset))
                 {
                     offset = Random.insideUnitCircle.normalized * 9f;
                 }
-                EnemyDumbAI enemy = Instantiate(enemyPrefab, player.transform.position + offset, Quaternion.identity);
+                EnemyDumbAI enemy = Instantiate(possibleEnemies[Random.Range(0, possibleEnemies.Count)], player.transform.position + offset, Quaternion.identity);
                 _activeEnemies.Add(enemy);
             }
         }
