9ed97d774a8a3f25eca3603c4527e445e38466fb
Thomas Presicci <thomaspresicci@gmail.com>
2023-04-16 15:07:19 -0500

    Added weaapon size upgrade

--- Assets/PlayerWeaponDamage.cs
+++ Assets/PlayerWeaponDamage.cs
@@ -6,6 +6,12 @@ public class PlayerWeaponDamage : MonoBehaviour
     [SerializeField] private int baseDamage;
     [SerializeField] private PlayerAttributes playerAttributes;
 
+    private void FixedUpdate()
+    {
+        var scale = playerAttributes.GetAttributeValue(AttributeType.WeaponSize);
+        transform.localScale = new Vector3(scale, scale, 1);
+    }
+
     public int GetDamage()
     {
         return (int) Math.Ceiling(baseDamage * playerAttributes.GetAttributeValue(AttributeType.Damage));
