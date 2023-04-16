5677ff21952f60ff437f2892342f725d36decfc8
Thomas Presicci <thomaspresicci@gmail.com>
2023-04-16 15:06:37 -0500

    Updated main menu buttons and background to be more in line with the current game scene

---
+++ Assets/MainMenuCamera.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuCamera : MonoBehaviour
+{
+    [SerializeField] private float cameraSpeed;
+    [SerializeField] private List<Transform> cameraPositions;
+    private int _count;
+
+    private void Start()
+    {
+        transform.position = cameraPositions[_count].position;
+        _count++;
+    }
+
+    private void MoveCamera()
+    {
+        _count++;
+        if (_count >= cameraPositions.Count)
+            _count = 0;
+        transform.position = cameraPositions[_count].position;
+        _count++;
+    }
+
+
+    void Update()
+    {
+        var target = cameraPositions[_count].position;
+        transform.position = Vector3.MoveTowards(transform.position, target, Time.deltaTime * cameraSpeed);
+        if (transform.position == target)
+        {
+            MoveCamera();
+        }
+    }
+}
