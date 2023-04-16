bd024d8ececbfb56484d808080a09ff27902d13c
Thomas Presicci <thomaspresicci@gmail.com>
2023-04-16 15:08:02 -0500

    Updated SceneTransition to work in builds

--- Assets/Scripts/SceneTransition.cs
+++ Assets/Scripts/SceneTransition.cs
@@ -1,12 +1,16 @@
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneTransition : MonoBehaviour
 {
-    [SerializeField] private SceneAsset targetScene;
+    [SerializeField] private int targetScene;
 
     public void LoadScene() {
-        SceneManager.LoadScene(targetScene.name);
+        SceneManager.LoadScene(targetScene);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
     }
 }
