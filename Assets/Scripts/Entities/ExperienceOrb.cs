2c15054b06903c252099221682436fbaa9d98c06
Thomas Presicci <thomaspresicci@gmail.com>
2023-04-29 17:11:47 -0500

    Experience orbs now get faster over time to avoid outrunning them

--- Assets/Scripts/Entities/ExperienceOrb.cs
+++ Assets/Scripts/Entities/ExperienceOrb.cs
@@ -3,35 +3,33 @@ using UnityEngine;
 public class ExperienceOrb : MonoBehaviour
 {
     private int _experienceReward;
-    private bool _activated;
     private PlayerExperience _target;
+    private float _speed;
 
     public void SetExperience(int experience)
     {
         _experienceReward = experience;
     }
-
-    public void Activate(PlayerExperience target)
-    {
-        _target = target;
-        _activated = true;
-    }
     
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (!col.CompareTag("ExperienceVacuum")) return;
-        _activated = true;
+        _speed = 5f;
         _target = col.transform.parent.GetComponent<PlayerExperience>();
     }
 
     private void Update()
     {
-        if (!_activated) return;
-        transform.position = Vector3.MoveTowards(transform.position, _target.transform.position, Time.deltaTime * 5f);
+        if (_speed == 0) return;
+        transform.position = Vector3.MoveTowards(transform.position, _target.transform.position, Time.deltaTime * _speed);
         if (transform.position == _target.transform.position)
         {
             _target.AddExperience(_experienceReward);
             Destroy(gameObject);
+        }
+        else
+        {
+            _speed += 0.03f;
         }
     }
 }
