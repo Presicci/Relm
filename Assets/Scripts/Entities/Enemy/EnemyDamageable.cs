f5989ed658b04e2659d2debfb564c3492a39dfbc
Thomas Presicci <thomaspresicci@gmail.com>
2023-05-05 12:26:50 -0500

    Fixed 'can not play a disabled audio source' error

--- Assets/Scripts/Entities/Enemy/EnemyDamageable.cs
+++ Assets/Scripts/Entities/Enemy/EnemyDamageable.cs
@@ -24,9 +24,9 @@ public class EnemyDamageable : Damageable
         if (!col.CompareTag("PlayerWeapon") && !projectile) return;
         PlayerWeaponDamage playerWeaponDamage = col.GetComponent<PlayerWeaponDamage>();
         if (playerWeaponDamage == null) return;
-        Damage(playerWeaponDamage.GetDamage());  
         //play sound
         _audioSource.Play();
+        Damage(playerWeaponDamage.GetDamage());
         _rigidbody2D.AddForce(forceMultiplier * (transform.position - (projectile ? col.transform.position : col.transform.parent.parent.position)).normalized, ForceMode2D.Impulse);
         if (col.CompareTag("PlayerProjectile"))
             Destroy(col.gameObject);
