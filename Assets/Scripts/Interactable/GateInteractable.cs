b4a062a5682b55c2ecf89fb04061785da0593592
Thomas Presicci <thomaspresicci@gmail.com>
2023-05-05 11:48:05 -0500

    Added FantasyIconsPackFree

---
+++ Assets/Scripts/Interactable/GateInteractable.cs
@@ -0,0 +1,7 @@
+public class GateInteractable : Interactable
+{
+    public override void Interact()
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Assets/Scripts/ScriptableObjects/AttributeItemObject.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObjects/Item")]
+public class AttributeItemObject : ScriptableObject
+{
+    [Tooltip("Inventory image.")]
+    public Sprite sprite;
+    
+    [Tooltip("Shown at the top of item tooltip.")]
+    public string itemName;
+    
+    [Tooltip("Shown at the bottom of item tooltip.")]
+    public string itemDescription;
+
+    [Tooltip("Does the item stack?")]
+    public bool stackable;
+}
